fbcb3e199f628d8f326837bdaf8cda3725c0da88
werbowym-sheridan <werbowym@sheridancollege.ca>
2025-01-17 23:49:54 -0500

    Initial Commit

---
+++ BusBuddy/BusBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusBuddyApp.swift
+//  BusBuddy
+//
+//  Created by Michael Werbowy on 2025-01-17.
+//
+
+import SwiftUI
+
+@main
+struct BusBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusBuddy
+//
+//  Created by Michael Werbowy on 2025-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
